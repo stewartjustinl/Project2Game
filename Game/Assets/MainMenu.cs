d5ad4968ac84d3e31e43fdf3462327000ee69a9e
Justin Stewart <stewartjustinl@gmail.com>
2020-11-05 19:55:45 -0800

    updated the game scene features. all links work, need to fix volume slider to work and add in remapping into controls menu

---
+++ Game/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // SceneManager.LoadScene("levelName"); // this option will be used if we want to manually tell the play button what scene to load
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // we will use this option if we want to setup loading scenes by indexing
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame!");
+        Application.Quit();
+    }
+}
