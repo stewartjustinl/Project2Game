92615aa1ad519860752ca302c35133c8f211e760
gingikid <bsumser@gmail.com>
2020-11-12 15:19:37 -0800

    added completed version of level

---
+++ Game/Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestCamera
+{
+    public class TopDownCamera : MonoBehaviour
+    {
+        public Transform m_Target;
+        public float m_Height = 10f;
+        public float m_Distance = 20f;
+        public float m_Angle = 45f;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected virtual void HandleCamera()
+        {
+
+        }
+    }
+}
