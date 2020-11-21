deb66cdd970b7858a00a13c2a2196c62595f6aa8
gingikid <bsumser@gmail.com>
2020-11-21 14:23:24 -0800

    merging changes from main into my branch, then merging back to main

---
+++ Game/Assets/Scripts/Nuker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuker : MonoBehaviour
+{
+    //script to delete empty room that spawns at start
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Block")) {
+            Destroy(other.gameObject);
+        }
+    }
+}
