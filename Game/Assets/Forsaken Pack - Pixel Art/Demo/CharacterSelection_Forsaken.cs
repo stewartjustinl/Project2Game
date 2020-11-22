8c8354d8fa400679bb76311f9de9cbd1f01fcafb
Justin Stewart <stewartjustinl@gmail.com>
2020-11-21 20:16:30 -0800

    adding player and enemy assets

---
+++ Game/Assets/Forsaken Pack - Pixel Art/Demo/CharacterSelection_Forsaken.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelection_Forsaken : MonoBehaviour {
+
+    [SerializeField] GameObject[] Characters;
+
+    private int currentSelection = 0;
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown("f"))
+        {
+            ChangeCharacter();
+        }
+	}
+
+    private void ChangeCharacter()
+    {
+        //Set current characters as inactive
+        Characters[currentSelection].SetActive(false);
+
+        currentSelection++;
+
+        if (currentSelection >= Characters.Length)
+        {
+            currentSelection = 0;
+        }
+
+        Characters[currentSelection].SetActive(true);
+    }
+}
---
+++ Game/Assets/Forsaken Pack - Pixel Art/Demo/DestroySelf_Forsaken.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySelf_Forsaken : MonoBehaviour {
+
+	void destroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
