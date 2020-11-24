34b7c0a6191f148227718aefe418a5952fa26cfa
Justin Stewart <stewartjustinl@gmail.com>
2020-11-23 21:39:00 -0800

    added more ui

---
+++ Game/Assets/Scripts/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+   public void MainMenu()
+   {
+   		SceneManager.LoadScene(0);
+   }
+
+   public void NewGame()
+   {
+   		SceneManager.LoadScene(1);
+   }
+
+   public void QuitGame()
+   {
+   		Debug.Log("QuitingGame!");
+   		Application.Quit();
+   }
+}
