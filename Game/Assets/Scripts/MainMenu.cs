94cb8dc296226fd6cae5b65b4907c21030fa7581
Justin Stewart <stewartjustinl@gmail.com>
2020-11-23 18:38:02 -0800

    added LevelCompleted Scene and scripts respectively

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -8,8 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        // SceneManager.LoadScene("levelName"); // this option will be used if we want to manually tell the play button what scene to load
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // we will use this option if we want to setup loading scenes by indexing
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
